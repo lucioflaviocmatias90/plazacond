95997aa7e833c2adb4ce6fdf0e7792cc8e7ada30
lucioflaviocmatias90 <lucioflavio.cmatias90@hotmail.com>
2020-09-15 23:29:56 -0300

    applying vuejs by cdn

--- frontend/dist/js/pages/dashboard2.js
+++ frontend/dist/js/pages/dashboard2.js
@@ -16,7 +16,7 @@ $(function () {
   var salesChartCanvas = $('#salesChart').get(0).getContext('2d')
 
   var salesChartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro'],
     datasets: [
       {
         label: 'Digital Goods',
@@ -27,18 +27,7 @@ $(function () {
         pointStrokeColor: 'rgba(60,141,188,1)',
         pointHighlightFill: '#fff',
         pointHighlightStroke: 'rgba(60,141,188,1)',
-        data: [28, 48, 40, 19, 86, 27, 90]
-      },
-      {
-        label: 'Electronics',
-        backgroundColor: 'rgba(210, 214, 222, 1)',
-        borderColor: 'rgba(210, 214, 222, 1)',
-        pointRadius: false,
-        pointColor: 'rgba(210, 214, 222, 1)',
-        pointStrokeColor: '#c1c7d1',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: [28, 48, 40, 19, 86, 27, 90, 78, 83]
       }
     ]
   }
